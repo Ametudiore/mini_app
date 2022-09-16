17cc4fc6f9878f6642d04d40f84055e0137c4597
Ametudiore <ametudiore@outlook.com>
2022-09-16 11:57:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Route} from 'react-router-dom';
+//import {BrowserRouter, Route, Switch,} from "react-router-dom";
+import './index.css'
+import NavbarComponent from './navbarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselComponent from './carouselComponent';
+import HomeComponent from './home';
+import FooterComponent from './footerComponent';
+import Error404 from './error404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <div className="App">
+      <header>
+         <NavbarComponent/>
       </header>
-    </div>
+        <Route>
+          <CarouselComponent/>
+        </Route>
+      
+        <Route>
+          <HomeComponent/>
+        </Route>
+     
+        <Route path="*">
+          <Error404/>
+        </Route>
+  
+
+      <FooterComponent/>
+
+  </div>
   );
 }
 
